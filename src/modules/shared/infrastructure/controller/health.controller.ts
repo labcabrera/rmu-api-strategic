58f8ee225c3fb6f679185c29c3ca34e9e99ad42a
labcabrera <lab.cabrera@gmail.com>
2026-02-19 08:29:02 +0100

    Updated multi-arch docker build.

--- src/modules/shared/infrastructure/controller/health.controller.ts
+++ src/modules/shared/infrastructure/controller/health.controller.ts
@@ -31,7 +31,7 @@ export class HealthController {
       () => this.memory.checkRSS('memory_rss', 350 * 1024 * 1024),
       async (): Promise<HealthIndicatorResult> => {
         const rmuApiCoreUri = this.config.get<string>('RMU_API_CORE_URI');
-        const healthUri = `${rmuApiCoreUri}/health`;
+        const healthUri = `${rmuApiCoreUri}/v1/health`;
         return await this.http.pingCheck('rmu-api-core', healthUri);
       },
     ]);
